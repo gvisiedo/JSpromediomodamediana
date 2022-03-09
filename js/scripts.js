e184642af89bf6a74aee62460408097e4a108058
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2022-03-09 19:49:42 +0100

    calculo de la moda

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,6 @@ const lista1 = [
     100,200,300,500,
 ];
 
-
-
-
-
-
 function calcularMediaAritmetica(lista){
    /* let sumaLista = 0;
 
@@ -24,4 +19,30 @@ const promedioLista= sumaLista/lista.length;
 
 return promedioLista;
 
+}
+
+const mitadLista1 = parseInt(lista1.length/2);
+
+function esPar(numero){
+    if (numero % 2 === 0){
+        return true
+    }else{
+        return false;
+    }
+}
+
+let mediana;
+
+if(esPar(lista1.length)){
+    const elemento1 = lista1[mitadLista1-1];
+    const elemento2 = lista1[mitadLista1];
+    const promedioElemento1y2= calcularMediaAritmetica([
+        elemento1,
+        elemento2,
+    ]);
+
+    mediana = promedioElemento1y2;
+
+}else{
+    mediana = lista1[mitadLista1];
 }
